59b7f6e1a80b3977f58dff40b72f45144ab3abd5
Nandan K <lodagaladeepak1012@gmail.com>
2026-07-22 17:36:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -224,4 +224,10 @@ Total:
   }
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
 module.exports = app;
